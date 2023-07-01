2afb25bb726981b9372cdf573360b8282d17e507
Ph1r1 <phiriyakorn_m@cmu.ac.th>
2023-07-01 12:57:47 +0700

    Step 5.6*

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
       <nav>
         <RouterLink to="/">Home</RouterLink> |
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About</RouterLink> |
+        <RouterLink to="/new-event">New Event</RouterLink>
       </nav>
   </header>
 
