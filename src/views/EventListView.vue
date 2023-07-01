ba0012dba13efb395a7c16de7e6ab6295e60f0a1
Ph1r1 <phiriyakorn_m@cmu.ac.th>
2023-07-01 14:04:11 +0700

    Step 6.7-6.8

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,43 +2,14 @@
 import EventCard from '../components/EventCard.vue'
 // import NewEventCard from '../components/NewEventCard.vue'
 import type { EventItem } from '@/type'
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
+import axios from 'axios'
 
-const events = ref<EventItem[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wales'
-  }
-])
+const events: Ref<Array<EventItem>> = ref([])
+
+axios.get<EventItem[]>('http://localhost:3004/events').then((response) => {
+  events.value = response.data
+})
 </script>
 
 <template>
