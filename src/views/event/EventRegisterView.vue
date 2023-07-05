1434344baffeb6b13416dd8f8ba0765758a914c3
ph1r1 <phiriyakorn_m@cmu.ac.th>
2023-07-05 13:01:15 +0700

    (Lab04) Step 1.4-1.5

--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
 import { type EventItem } from '@/type'
 import type { PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
-defineProps({
+const props = defineProps({
   event: {
     type: Object as PropType<EventItem>,
     require: true
   }
 })
+
+const router = useRouter()
+const store = useMessageStore()
+
+const register = () => {
+  store.updateMessage('You are successfully registered for ' + props.event?.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({
+    name: 'event-detail',
+    params: {
+      id: props.event?.id
+    }
+  })
+}
 </script>
 
 <template>
   <p>Registration form here</p>
+  <button @click="register">Register Me</button>
 </template>
