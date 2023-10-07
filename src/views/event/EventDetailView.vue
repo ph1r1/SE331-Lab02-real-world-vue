44af9540f9767e1d9c89a830aa20144702ecd6cb
webwebweb5 <phiriyakornman@gmail.com>
2023-10-07 23:44:56 +0700

    (Lab11) 2.8*

--- src/views/event/EventDetailView.vue
+++ src/views/event/EventDetailView.vue
@@ -21,5 +21,8 @@ defineProps({
     </div>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
+    <div class="flex flex-row flex-wrap justify-center">
+      <img v-for="image in event.images" :key="image" :src="image" alt="events image" class="border-solid border-gray-200 border-2 rounded p-1 m-1 w-40 hover:shadow-lg" />
+    </div>
   </div>
 </template>
